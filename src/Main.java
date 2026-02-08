7f8fce986e652ba0d633b05d17a953d722b07490
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-08 10:12:00 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+package main;
+
+import model.*;
+import payment.*;
+import delivery.*;
+import notification.*;
+import discount.*;
+
+import java.util.List;
 
 public class Main {
+    public static void main(String[] args) {
+
+        Order order = new Order();
+
+        order.addItem(new Product("Laptop", 350000), 1);
+        order.addItem(new Product("Mouse", 8000), 2);
+
+        order.setPayment(new CreditCardPayment());
+        order.setDelivery(new CourierDelivery());
+
+        DiscountCalculator calculator = new DiscountCalculator(
+                List.of(new SeasonDiscount(), new BigOrderDiscount())
+        );
+
+        double total = order.calculateTotal(calculator);
+
+        order.pay(total);
+        order.deliver();
+
+        INotification notification = new EmailNotification();
+        notification.sendNotification("Order completed. Total: " + total);
+    }
 }
