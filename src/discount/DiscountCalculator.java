b01975454738c3510c365456f6c7846b17cd8a0d
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-08 11:00:46 +0500

    Update DiscountCalculator.java

--- src/discount/DiscountCalculator.java
+++ src/discount/DiscountCalculator.java
@@ -1,4 +1,20 @@
 package discount;
 
+import java.util.List;
+
 public class DiscountCalculator {
+
+    private final List<IDiscountRule> rules;
+
+    public DiscountCalculator(List<IDiscountRule> rules) {
+        this.rules = rules;
+    }
+
+    public double applyDiscount(double total) {
+        double result = total;
+        for (IDiscountRule rule : rules) {
+            result = rule.apply(result);
+        }
+        return result;
+    }
 }
