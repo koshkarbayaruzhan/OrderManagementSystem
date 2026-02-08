c8d7472bd9f0358333e1d3f4e5d3f736fe6e8a8b
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-08 11:00:30 +0500

    Update BigOrderDiscount.java

--- src/discount/BigOrderDiscount.java
+++ src/discount/BigOrderDiscount.java
@@ -1,4 +1,8 @@
 package discount;
 
-public class BigOrderDiscount {
+public class BigOrderDiscount implements IDiscountRule {
+    public double apply(double total) {
+        if (total > 100000) return total * 0.85;
+        return total;
+    }
 }
