1a290953c6e6f61fc5541dcedf52f76cccc81d3c
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-08 10:12:30 +0500

    Update Product.java

--- src/model/Product.java
+++ src/model/Product.java
@@ -1,4 +1,12 @@
 package model;
 
 public class Product {
+    public String name;
+    public double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
 }
+
