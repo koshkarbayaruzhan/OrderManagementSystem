2d293348afe04552bff28e021d3521b038df66e9
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-08 10:59:33 +0500

    Update SmsNotification.java

--- src/notification/SmsNotification.java
+++ src/notification/SmsNotification.java
@@ -1,4 +1,7 @@
 package notification;
 
-public class SmsNotification {
+public class SmsNotification implements INotification {
+    public void sendNotification(String message) {
+        System.out.println("SMS: " + message);
+    }
 }
